b600c8abe025702582e6a193efffe144e9f75c4d
Michael Martin <mwm.cal@gmail.com>
2015-05-05 11:48:11 -0700

    Updating docker-up.sh with --net host --rm

--- working/docker-up.sh
+++ working/docker-up.sh
@@ -1,1 +1,1 @@
-docker run -v ~/working/share:/share -i -t sequenceiq/hadoop-docker:2.6.0 /etc/bootstrap.sh -bash
+docker run --net host --rm -v ~/working/share:/share -i -t sequenceiq/hadoop-docker:2.6.0 /etc/bootstrap.sh -bash
